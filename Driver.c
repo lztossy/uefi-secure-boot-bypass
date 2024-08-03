d568cf8ed0716f02c207f189c6d705b5b9301799
tos <164348607+lztossy@users.noreply.github.com>
2024-08-03 22:55:31 +1000

    Update Driver.c

--- Driver.c
+++ Driver.c
@@ -4,10 +4,8 @@
 #include <Library/DebugLib.h>
 #include <Library/BaseMemoryLib.h>
 
-// Original GetVariable function pointer
 EFI_GET_VARIABLE OriginalGetVariable = NULL;
 
-// Hooked GetVariable function
 EFI_STATUS EFIAPI HookedGetVariable(
     IN CHAR16* VariableName,
     IN EFI_GUID* VendorGuid,
@@ -17,9 +15,8 @@ EFI_STATUS EFIAPI HookedGetVariable(
 )
 {
     if (VariableName != NULL && StrCmp(VariableName, L"SecureBoot") == 0) {
-        // Simulate Secure Boot being enabled
         if (*DataSize >= sizeof(UINT8)) {
-            *((UINT8*)Data) = 1; // Secure Boot enabled
+            *((UINT8*)Data) = 1; 
             if (Attributes != NULL) {
                 *Attributes = EFI_VARIABLE_BOOTSERVICE_ACCESS | EFI_VARIABLE_RUNTIME_ACCESS | EFI_VARIABLE_NON_VOLATILE;
             }
@@ -32,28 +29,22 @@ EFI_STATUS EFIAPI HookedGetVariable(
         }
     }
 
-    // Call the original GetVariable function for other variables
     return OriginalGetVariable(VariableName, VendorGuid, Attributes, DataSize, Data);
 }
 
-// Entry point of the UEFI driver
 EFI_STATUS EFIAPI UefiMain(IN EFI_HANDLE ImageHandle, IN EFI_SYSTEM_TABLE* SystemTable)
 {
     EFI_STATUS Status;
 
-    // Save the original GetVariable function pointer
     OriginalGetVariable = gRT->GetVariable;
 
-    // Hook the GetVariable function
     gRT->Hdr.CRC32 = 0;
     gRT->GetVariable = HookedGetVariable;
     gRT->Hdr.CRC32 = gST->Hdr.CRC32; // Recalculate the CRC32 checksum
 
-    // Driver initialization successful
     return EFI_SUCCESS;
 }
 
-// UEFI Driver entry point
 EFI_STATUS
 EFIAPI
 InitializeSecureBootHook(
